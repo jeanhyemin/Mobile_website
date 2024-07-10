c861d568263c3866ef0ff3bcf0cd961accce29df
hyemin <6jeonhyemin9@gmail.com>
2024-07-10 16:42:27 +0900

    mobile 최초업로드

---
+++ js_popup/click_pop0/layer_popup.js
@@ -0,0 +1,16 @@
+ $(document).ready(function(){
+     
+      $('.openBtn').on('click', function(e){
+          e.preventDefault();
+          
+          $(this).next('.big').fadeIn('slow');
+          $('.box').show();
+      });
+     
+     $('.closeBtn, .box').on('click', function(e){
+          e.preventDefault();
+
+          $('.big').fadeOut('fast');
+          $('.box').hide();
+     });
+ });
